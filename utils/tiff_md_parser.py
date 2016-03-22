c61d4c9cd67b1a78d68056dbeecbe1b33d6ac36f
CJ-Wright <cjwright4242@gmail.com>
2016-03-22 09:38:58 -0400

    STY: cleanup tiff metadata parser

--- utils/tiff_md_parser.py
+++ utils/tiff_md_parser.py
@@ -1,26 +1,24 @@
 import ConfigParser
 import time
+from datetime import datetime
 
 def parse_tif_metadata(file):
     config = ConfigParser.ConfigParser()
     config.read(file)
-    ouput_dict = {}
+    output_dict = {}
     for section in config.sections():
         for option in config.options(section):
             try:
-                ouput_dict[option] = float(config.get(section=section,
+                output_dict[option] = float(config.get(section=section,
                                                   option=option))
             except ValueError:
-                ouput_dict[option] = config.get(section=section,
+                output_dict[option] = config.get(section=section,
                                                   option=option)
-    # if 'datestring' in ouput_dict.keys():
-    #     pattern = '%Y.%m.%d : %H:%M:%S.%f'
-    #     epoch = int(time.mktime(time.strptime(ouput_dict['datestring'], pattern)))
-    #     ouput_dict['epoch_time'] = epoch
-    return ouput_dict
+    if 'datestring' in output_dict.keys():
+        pattern = '%Y.%m.%d : %H:%M:%S.%f'
+        output_dict['time_from_date'] = datetime.strptime(output_dict['datestring'],
+                                                        pattern)
+    if 'timestamp' in output_dict.keys():
+        output_dict['time'] = datetime.fromtimestamp(output_dict['timestamp'])
+    return output_dict
 
-
-if __name__ == '__main__':
-    f = '/run/media/christopher/Seagate Backup Plus Drive/merge_backup/Documents/Research/Brown/Sun_Lab/PDF/PDFdata/July/X17A/1/NiPd_1/NiPd_1-00031.tif.metadata'
-
-    print parse_tif_metadata(f)
