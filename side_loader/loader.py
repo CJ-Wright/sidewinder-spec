494d6264c4a341974aac9d350318d9558ee302f3
CJ-Wright <cjwright4242@gmail.com>
2016-03-22 11:01:30 -0400

    WIP: start building the loader

--- side_loader/loader.py
+++ side_loader/loader.py
@@ -1,12 +1,45 @@
+"""
+The plan:
+1. Load up all the SPEC metadata
+2. Load each run_start configuration file, which we write currently in each dir
+3. Create the run_start document which should include:
+    [General]
+    starttime
+    [Distance 1]
+    Distance
+    File_Stem
+    StartMask
+    Configuration_img (fs_uuid?)
+    Configuration_poni(fs_uuid?) (also write a pyFAI.geo handler)
+    Background_img (File or folder if T dependent) (fs_uuid?)
+    Background_chi (File or folder if T dependent) (fs_uuid?)
+4. Make the data descriptors
+5. Insert the data that goes with the image
+6. Insert runstop
+7. Repeat for all data sets
+
+"""
 from metadatastore.api import insert_event, insert_descriptor, insert_run_start, insert_run_stop
 from uuid import uuid4
 from filestore.api import insert_resource, insert_datum, register_handler
 from filestore.handlers_base import HandlerBase
+import numpy as np
+from utils.spec_parser_11_id_b import
 
+class TiffHandler(HandlerBase):
+    specs = {'TIFF'} | HandlerBase.specs
 
+    def __init__(self, name):
+        self._name = name
+
+    def __call__(self):
+        return tifffile.read(self._name)
+
+register_handler('TIFF', TiffHandler)
+# for each "run" which we usually call a folder
 run_start_uid = insert_run_start(time=SPEC_START_TIME, scan_id=1,
                                  beamline_id='example',
-                                 uid=str((uuid4()), **kwargs)
+                                 uid=str((uuid4()), **kwargs))
 
 data_keys1 = {'I0': dict(source='IO', dtype='number'),
               'img': dict(source='det', dtype='array', shape=(2048, 2048), external='FILESTORE:')}
@@ -14,11 +47,11 @@ data_keys2 = {'T': dict(source='T', dtype='number'),}
 
 descriptor1_uid = insert_event_descriptor(
     run_start=run_start_uid, data_keys=data_keys1, time=0.,
-    uid=str(uuid4())
+    uid=str(uuid4()))
 
 descriptor2_uid = insert_event_descriptor(
     run_start=run_start_uid, data_keys=data_keys2, time=0.,
-    uid=str(uuid4())
+    uid=str(uuid4()))
 
 # read in the temperature data up here
 #temperature_data, time_data = read_temp_data()
@@ -43,20 +76,7 @@ for idx, (img_name, I, timestamp) in enumerate(zip(img_names, I0, timestamps)):
 
 insert_run_stop(run_start=run_start_uid, time=np.max(timestamps), uid=str(uuid4()))
 
-class TiffHandler(HandlerBase):
-    specs = {'TIFF'} | HandlerBase.specs
-
-    def __init__(self, name):
-        self._name = name
-
-    def __call__(self):
-        return tifffile.read(self._name)
-
-
-
-
 from databroker import db, get_events
-register_handler('TIFF', TiffHandler)
 hdr = db[-1]
 events = get_events(hdr)
 ev0 = next(events)
@@ -68,7 +88,7 @@ ev0['data']['img']  # array data
 # this returns "headers" which are:
 # header = {'start': RunStart, 'descriptors': [Descriptor1, Descriptor2], 'stop': RunStop}
 #header.descriptors[0]
-db[] # search for run_numbers, uids and slicing
+#db[] # search for run_numbers, uids and slicing
 db[10] # scan 10
 db[-5] # 5 ago
 db['10ssdf93'] # uid
