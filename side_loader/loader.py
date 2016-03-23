c11d858894c2dd7a94ecc2fdf58e82f3f8d83a68
CJ-Wright <cjwright4242@gmail.com>
2016-03-23 10:33:48 -0400

    WIP: write loader for temperature data, I don't know how to work with sample changer data yet

--- side_loader/loader.py
+++ side_loader/loader.py
@@ -15,16 +15,18 @@ The plan:
     Background_chi (File or folder if T dependent) (fs_uuid?)
 4. Make the data descriptors
 5. Insert the data that goes with the image
+5a. Match data with SPEC metadata
 6. Insert runstop
 7. Repeat for all data sets
 
 """
-from metadatastore.api import insert_event, insert_descriptor, insert_run_start, insert_run_stop
+from metadatastore.api import insert_event, insert_descriptor, \
+    insert_run_start, insert_run_stop
 from uuid import uuid4
 from filestore.api import insert_resource, insert_datum, register_handler
 from filestore.handlers_base import HandlerBase
 import numpy as np
-from utils.spec_parser_11_id_b import
+
 
 class TiffHandler(HandlerBase):
     specs = {'TIFF'} | HandlerBase.specs
@@ -35,65 +37,90 @@ class TiffHandler(HandlerBase):
     def __call__(self):
         return tifffile.read(self._name)
 
+
 register_handler('TIFF', TiffHandler)
-# for each "run" which we usually call a folder
-run_start_uid = insert_run_start(time=SPEC_START_TIME, scan_id=1,
-                                 beamline_id='example',
-                                 uid=str((uuid4()), **kwargs))
 
-data_keys1 = {'I0': dict(source='IO', dtype='number'),
-              'img': dict(source='det', dtype='array', shape=(2048, 2048), external='FILESTORE:')}
-data_keys2 = {'T': dict(source='T', dtype='number'),}
+if __name__ == '__main__':
+    import os
+    from utils.spec_parser_11_id_b import parse_spec_file
+    from utils.tiff_md_parser import parse_tif_metadata
 
-descriptor1_uid = insert_event_descriptor(
-    run_start=run_start_uid, data_keys=data_keys1, time=0.,
-    uid=str(uuid4()))
+    # 1. Load up all the SPEC metadata
+    spec_file_loc = ''
+    spec_Data = parse_spec_file(spec_file_loc)
 
-descriptor2_uid = insert_event_descriptor(
-    run_start=run_start_uid, data_keys=data_keys2, time=0.,
-    uid=str(uuid4()))
+    # 2. Load each run_start configuration file, which we write currently in each dir
+    insert_folder = ''
+    config_file = os.path.join(insert_folder, 'config.txt')
+    kwargs = 'GIANT BLOB FROM CONFIG FILE'
 
-# read in the temperature data up here
-#temperature_data, time_data = read_temp_data()
+    # Load all the metadata files in the folder
+    metadata_files = [os.path.join(insert_folder, f) for f in os.listdir() if
+                      f.endswith('.tif.metadata')]
+    metadata_data = [parse_tif_metadata(f) for f in metadata_files]
 
-# insert all the temperature data
-for idx, (temp, t) in enumerate(zip(temperature_data, time_data)):
-    insert_event(descriptor=descriptor2_uid, time=t, data={'T': temp}, uid=str(uuid4()),
-                 timestamps={'T': t}, seq_num=idx)
+    # Sort the folder's data by time so we can have the start time
+    timestamp_list = [f['timestamp'] for f in metadata_data]
 
-# read in spec data
-#img_names, I0, timestamps = read_spec_data()
+    run_start_uid = insert_run_start(time=min(timestamp_list), scan_id=1,
+                                     beamline_id='example',
+                                     uid=str((uuid4()), **kwargs))
 
-for idx, (img_name, I, timestamp) in enumerate(zip(img_names, I0, timestamps)):
-    fs_uid = uui4()
-    resource = insert_resource('TIFF', img_name)
-    insert_datum(resource, fs_uid)
-    data = {'img': fs_uid, 'I0': I}
-    timestamps = {'img': timestamp, 'I0': timestamp}
-    insert_event(descriptor=descriptor1_uid, time=timestamp, data=data,
-                 uid=str(uuid4()), timestamps=timestamps, seq_num=idx)
+    data_keys1 = {'I0': dict(source='IO', dtype='number'),
+                  'img': dict(source='det', dtype='array', shape=(2048, 2048),
+                              external='FILESTORE:')}
+    data_keys2 = {'T': dict(source='T', dtype='number'),}
 
+    descriptor1_uid = insert_event_descriptor(
+        run_start=run_start_uid, data_keys=data_keys1, time=0.,
+        uid=str(uuid4()))
 
-insert_run_stop(run_start=run_start_uid, time=np.max(timestamps), uid=str(uuid4()))
+    descriptor2_uid = insert_event_descriptor(
+        run_start=run_start_uid, data_keys=data_keys2, time=0.,
+        uid=str(uuid4()))
 
-from databroker import db, get_events
-hdr = db[-1]
-events = get_events(hdr)
-ev0 = next(events)
-ev0['data']['img']  # array data
+    # read in all the remaining data
+    # sort remaining data by time
 
+    # insert all the temperature data
+    for idx, (temp, t) in enumerate(zip(temperature_data, time_data)):
+        insert_event(descriptor=descriptor2_uid, time=t, data={'T': temp},
+                     uid=str(uuid4()),
+                     timestamps={'T': t}, seq_num=idx)
 
+    # read in spec data
+    # img_names, I0, timestamps = read_spec_data()
 
-# databroker has two methods of accessing data
-# this returns "headers" which are:
-# header = {'start': RunStart, 'descriptors': [Descriptor1, Descriptor2], 'stop': RunStop}
-#header.descriptors[0]
-#db[] # search for run_numbers, uids and slicing
-db[10] # scan 10
-db[-5] # 5 ago
-db['10ssdf93'] # uid
-db[5:15]
-db[-10:-5]
+    for idx, (img_name, I, timestamp) in enumerate(
+            zip(img_names, I0, timestamps)):
+        fs_uid = uui4()
+        resource = insert_resource('TIFF', img_name)
+        insert_datum(resource, fs_uid)
+        data = {'img': fs_uid, 'I0': I}
+        timestamps = {'img': timestamp, 'I0': timestamp}
+        insert_event(descriptor=descriptor1_uid, time=timestamp, data=data,
+                     uid=str(uuid4()), timestamps=timestamps, seq_num=idx)
 
-db() # takes kwargs that should match top level keys in the RunStart
-db(start_time='2016-01-01', stop_time='2016-02-01')
+    insert_run_stop(run_start=run_start_uid, time=np.max(timestamps),
+                    uid=str(uuid4()))
+
+    from databroker import db, get_events
+
+    hdr = db[-1]
+    events = get_events(hdr)
+    ev0 = next(events)
+    ev0['data']['img']  # array data
+
+    # databroker has two methods of accessing data
+    # this returns "headers" which are:
+    # header = {'start': RunStart, 'descriptors': [Descriptor1, Descriptor2], 'stop': RunStop}
+    # header.descriptors[0]
+    # db[] # search for run_numbers, uids and slicing
+    db[10]  # scan 10
+    db[-5]  # 5 ago
+    db['10ssdf93']  # uid
+    db[5:15]
+    db[-10:-5]
+
+    db()  # takes kwargs that should match top level keys in the RunStart
+    db(start_time='2016-01-01', stop_time='2016-02-01')
