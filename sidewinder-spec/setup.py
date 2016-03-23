04af858611fb54fae463049a4c9b44ecf9b9a386
CJ-Wright <cjwright4242@gmail.com>
2016-03-23 11:39:53 -0400

    BLD: new setup.py

---
+++ sidewinder-spec/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='sidewinder-spec',
+    version='',
+    packages=['', 'utils', 'side_loader'],
+    package_dir={'': 'sidewinder-spec'},
+    url='',
+    license='',
+    author='Christopher J. Wright',
+    author_email='wright1@email.sc.edu',
+    description=''
+)
