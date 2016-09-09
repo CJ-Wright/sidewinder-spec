834eb7c282223c07989f10b5e58846fef2de9cb2
christopher <cjwright4242@gmail.com>
2016-09-09 13:41:19 -0400

    ENH: fix tests

--- sidewinder_spec/tests/test_parsers.py
+++ sidewinder_spec/tests/test_parsers.py
@@ -1,15 +1,18 @@
 from sidewinder_spec.utils.parsers import parse_new_spec_file, \
     parse_new_spec_run
 from pprint import pprint
+import os
 
+spec_file = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                         'spec.log')
 
 def test_parse_new_spec_file():
-    a = parse_new_spec_file('spec.log')
+    a = parse_new_spec_file(spec_file)
     print(a)
 
 
 def test_parse_new_spec_run():
-    a = parse_new_spec_file('spec.log')
+    a = parse_new_spec_file(spec_file)
     b = parse_new_spec_run(a)
     pprint(b)
 
