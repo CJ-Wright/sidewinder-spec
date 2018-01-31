92e8a85c9ee3d28c6d234f1d161921dae304b299
christopher <cjwright4242@gmail.com>
2018-01-31 16:42:06 -0500

    FIX: parser puts out all images

--- sidewinder_spec/pal_xfel/parser.py
+++ sidewinder_spec/pal_xfel/parser.py
@@ -27,65 +27,97 @@ def parse_hdf5(fn):
     suid = new_uid()
     # loop through the scans
     for scans in f.keys():
-        # TODO: put in calibration parameters
+        # Create start doc
         start_doc = {
             'uid': suid,
             'shot_number': shot_number,
             'i0thresh': 2.e9,
             'i0amp': 1.e10,
             'calibration_md': calib_config_dict,
-            'time': time.time()
+            'time': time.time(),
+            'sideloaded': True,
+            'composition_string': 'Au',
+            'experimenters': ['Tadesse', 'Assefa', 'Jane', 'Doe', 'Richard',
+                              'Roe'],
+            'bt_wavelength': (12.398 / 9.70803 * 1.0e-10)
         }
         yield 'start', start_doc
+        # Create most of the descriptor
         duid = new_uid()
         descriptor_doc = {'uid': duid,
                           'name': 'primary',
                           'run_start': suid,
                           'data_keys': {'delay_timestamp': {
                               'source': 'delay_stage', 'dtype': 'float',
-                              'shape': [], 'unit': 'ps'}},
+                              'shape': [], 'unit': 'ps'},
+                              'shot_number': {
+                                  'source': 'sidewinder', 'dtype': 'int',
+                                  'shape': [], 'unit': 'NA'}},
                           'time': time.time()}
         yielded_descriptor = False
+
         # loop through the delay points
         for i, delay_point in enumerate(f[scans].keys()):
-            data_dict = {}
-            data_timestamps = {}
-            for data_source in [ff for ff in f[format_keys(scans, delay_point)].keys() if ff in ['RayMX', 'photodiode']]:
-                timestamp_name = list(
-                    f[format_keys(scans, delay_point,
-                                  data_source)].keys())[shot_number]
-                k = format_keys(scans, delay_point, data_source,
-                                timestamp_name)
-                ts = timestamp_name.strip('timestamp_')
-                v = f[k].value
-                if data_source not in descriptor_doc['data_keys']:
+            events = []
+
+            timestamps = list(
+                f[format_keys(scans, delay_point, 'RayMX')].keys())
+
+            # loop through the shots per delay point
+            for shot_number, timestamp in enumerate(timestamps):
+                ts = timestamp.strip('timestamp_')
+                event = {'uid': new_uid(),
+                         'descriptor': duid,
+                         'filled': {'image': True},
+                         'data': {'shot_number': shot_number},
+                         'timestamps': {'shot_number': ts},
+                         'seq_num': i,
+                         'time': time.time(),
+                         }
+                # loop through the detectors
+                for data_source in [ff for ff in
+                                    f[format_keys(scans, delay_point)].keys()
+                                    if
+                                    ff in ['RayMX', 'photodiode']]:
+
+                    data_key = format_keys(scans, delay_point, data_source,
+                                           timestamp)
+                    v = f[data_key].value
+
+                    # fill in missing descriptor info
                     if isinstance(v, np.ndarray):
                         s = v.shape
-                        if s == (1, ):
+                        if s == (1,):
                             v = float(v)
                             s = []
                         else:
                             v = v.astype('float32')
                     else:
                         s = []
-                data_dict[key_data_map[data_source]] = v
-                data_timestamps[key_data_map[data_source]] = int(ts)
-                if not yielded_descriptor:
-                    dtype = str(getattr(v, 'dtype', type(v)))
-                    descriptor_doc['data_keys'][key_data_map[data_source]] = {
-                        'source': data_source,
-                        'dtype': dtype,
-                        'shape': s}
+
+                    event['data'][key_data_map[data_source]] = v
+                    event['timestamps'][key_data_map[data_source]] = int(ts)
+
+                    if not yielded_descriptor:
+                        dtype = str(getattr(v, 'dtype', type(v)))
+                        descriptor_doc['data_keys'][
+                            key_data_map[data_source]] = {
+                            'source': data_source,
+                            'dtype': dtype,
+                            'shape': s}
+                events.append(event)
+
             if not yielded_descriptor:
                 yielded_descriptor = True
                 yield 'descriptor', descriptor_doc
-            yield 'event', {'uid': new_uid(),
-                            'descriptor': duid,
-                            'data': data_dict,
-                            'timestamps': data_timestamps,
-                            'filled': {'image': True},
-                            'seq_num': i,
-                            'time': time.time()}
+
+            # yield the events
+            for i, e in enumerate(events):
+                print(i, e['data']['shot_number'])
+                pprint(e)
+                assert i == e['data']['shot_number']
+                yield 'event', e
+
         yield 'stop', {'uid': new_uid(), 'run_start': suid,
                        'time': time.time()}
 
@@ -93,7 +125,7 @@ def parse_hdf5(fn):
 if __name__ == '__main__':
     # from bluesky.callbacks.broker import LiveImage
     from databroker.broker import Broker
-    import matplotlib.pyplot as plt
+    # import matplotlib.pyplot as plt
 
     # li = LiveImage('image', cmap='viridis',
     #                limit_func=lambda im: (
@@ -127,4 +159,3 @@ if __name__ == '__main__':
         # plt.pause(1)
         # if n == 'event':
         #     print(d['data']['image'].shape)
-    print(db[-1])
