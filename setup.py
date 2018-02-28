775ddff19d86e336ed3e7b9ab4fea16b1122389d
christopher <cjwright4242@gmail.com>
2018-02-28 15:40:33 -0500

    FIX: scripts in setup

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     url='',
     license='',
     author='Christopher J. Wright',
-    author_email='wright1@email.sc.edu',
-    description=''
+    author_email='cjwright4242@gmail.com',
+    description='',
+    scripts=['scripts/sidewind']
 )
