6550bef9c64f949ddefa539a016320018f20485d
christopher <cjwright4242@gmail.com>
2018-03-01 17:33:19 -0500

    TST: make counter for test

--- sidewinder_spec/tests/test_nomad.py
+++ sidewinder_spec/tests/test_nomad.py
@@ -4,7 +4,9 @@ from sidewinder_spec.nomad import parse
 
 def test_nomad_parser():
     fp = rs_fn('sidewinder_spec', 'data/nomad')
+    c = 0
     for n, d in parse(fp):
         assert n in {'start', 'descriptor', 'event', 'stop'}
         assert isinstance(d, dict)
-    assert n
+        c = 1
+    assert c == 1
