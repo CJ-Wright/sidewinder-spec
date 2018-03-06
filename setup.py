073a01f987c76f608c3b7d70edd28620c5f500d5
christopher <cjwright4242@gmail.com>
2018-03-06 09:55:35 -0500

    bumped version to 0.0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='shed-sidewinder',
-    version='0.0.4',
+    version='0.0.5',
     packages=find_packages(),
     author='Christopher J. Wright',
     author_email='cjwright4242@gmail.com',
